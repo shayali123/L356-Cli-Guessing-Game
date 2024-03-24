ca107e1a9fba533d4c88f0d40c3fcc73a2736633
shayali123 <160710648+shayali123@users.noreply.github.com>
2024-03-24 12:05:44 +0500

    Push My Calculator To Github

---
+++ main.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+let random = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        message: "Please guess a number 1-10 :",
+        type: "number",
+        name: "userguessednumber",
+    },
+]);
+if (answer.userguessednumber === random) {
+    console.log("Congratulation, you guessed a right number");
+}
+else {
+    console.log("You guessed a wrong number");
+}
